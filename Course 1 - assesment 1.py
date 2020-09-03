67b1d00ea1084dcebcda9925f0b1ceac573dd1a1
Srbhmishra18 <Srbhmishra18@gmail.com>
2020-09-03 20:46:39 +0530

    Add files via upload

---
+++ Course 1 - assesment 1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # course_1_assessment_1
+intro-12-1: Why create an algorithm?
+A. Because computers are better at solving problems.
+B. So that you don't have to solve the problem yourself.
+C. So that you have a general solution to a problem.
+D. Because you need a set of instructions to follow.Ans = C
+# 2  - Write code to print out the phrase “Hello World”.
+
+# In[1]:
+
+
+print("Hello World")
+
